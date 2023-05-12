0525fd31a42582c93ff7f6a667e5f760771f645e
Dhanarajb <88299676+Dhanarajb@users.noreply.github.com>
2023-05-12 08:02:53 +0530

    1 file modified

--- script.js
+++ script.js
@@ -27,3 +27,49 @@ const age1 = function (ag) {
 };
 const age11 = age1(1996);
 console.log(age11);
+
+// Arrow Function
+
+const findName = (inName) => inName;
+console.log(findName('Dhanaraj'));
+
+const findAgee = (agee) => 2037 - agee;
+console.log(findAgee(1997));
+
+// -----------------------------------------------------------------------
+
+function Avg_Cal(in1) {
+  return in1 * 4;
+}
+
+function fruits(banana, orange) {
+  const Cal_banana = Avg_Cal(banana);
+  const Cal_orange = Avg_Cal(orange);
+  const Cal_both = Cal_banana + Cal_orange;
+  return Cal_both;
+}
+
+const result = fruits(4, 0);
+console.log(result);
+// ---------------------------------------------------------------------------
+
+const CalcAverage = (a, b, c) => (a + b + c) / 3;
+
+const AvgDolphin = CalcAverage(23, 23, 45);
+const AvgKoalas = CalcAverage(43, 23, 23);
+
+function CheckWinners(AvgDolphin, AvgKoalas) {
+  if (AvgDolphin > AvgKoalas) {
+    console.log('Dolphin Win');
+  } else if (AvgDolphin < AvgKoalas) {
+    console.log('Koalas Win');
+  } else if (AvgDolphin === AvgKoalas) {
+    console.log('Together Win');
+  } else {
+    console.log('InValid');
+  }
+}
+
+const res = CheckWinners(AvgDolphin, AvgKoalas);
+console.log(res);
+// const CalcAverage = (23+34+45)/3
